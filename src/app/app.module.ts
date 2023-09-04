6af8073edda92d6fa802231ff8dbd38df45c15cf
AngelTravieso <angeltraviesoc@gmail.com>
2023-09-04 10:55:55 -0400

    Navbar component creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CharactersModule } from './characters/characters.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { CharactersModule } from './characters/characters.module';
   imports: [
     BrowserModule,
     CharactersModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
